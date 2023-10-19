bf6ce5dd7cb4384a18aff6de45a9d52c61fb4849
Alysson Mascarenhas <alymascarenhas31@gmail.com>
2023-10-19 09:31:08 -0300

    refactor: corrigindo numeros magicos

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -24,18 +24,21 @@ export class CardComponent {
   @Input() public config!: CardProps;
   @Output() public reload: EventEmitter<boolean> = new EventEmitter();
 
-  public click(): void {
-    this.reload.emit(true);
-  }
+  private readonly CRITICAL_VALUE = 1;
+  private readonly IMPORTANT_VALUE = 5;
 
   public setCardClass(): CardCSSClasses {
     if (this.config && this.config.value) {
-      return this.config.value <= 1
+      return this.config.value <= this.CRITICAL_VALUE
         ? 'text-red'
-        : this.config.value > 5
+        : this.config.value > this.IMPORTANT_VALUE
         ? 'text-blue'
         : 'text-green';
     }
     return 'no-class';
+  }
+
+  public click(): void {
+    this.reload.emit(true);
   }
 }
